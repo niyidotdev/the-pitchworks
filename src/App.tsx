b33f3341d6aaed3bc246c7390f63f9864e375545
niyi <akandefortunatus2021@gmail.com>
2026-02-16 20:04:50 +0100

    feat: hero design

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,78 @@
 import Navbar from "./components/navbar";
+import Sparkle from "./assets/icons/sparkle.svg?react";
+import Marquee from "react-fast-marquee";
 
 function App() {
   return (
-    <div className="main-background relative flex min-h-screen flex-col items-center justify-center p-8 text-center">
-      <Navbar />
-      {/* <div className="mt-20 flex flex-col items-center">
-        <h1 className="mb-4 bg-linear-to-r from-white to-white/70 bg-clip-text text-6xl font-bold text-transparent">
-          PitchWorks
-        </h1>
-        <p className="max-w-2xl text-xl text-white/80 opacity-80">
-          Your Creative Partner Helping Businesses Scale With Confidence.
-        </p>
-      </div> */}
-    </div>
+    <section>
+      <div className="main-background relative flex min-h-screen flex-col justify-center p-4 md:px-6">
+        <Navbar />
+
+        {/* Hero section */}
+        {/* <section className="mx-auto flex w-[96%] flex-col items-center justify-between gap-8 py-28 md:flex-row"> */}
+        <section className="mx-auto my-28 grid w-[96%] grid-cols-1 gap-6 md:grid-cols-2">
+          <div className="max-w-4xl">
+            <div className="flex max-w-fit items-center gap-2 rounded-full border-white/10 bg-[linear-gradient(153.09deg,rgba(255,255,255,0.12)_16.83%,rgba(255,255,255,0.08)_85.44%)] px-4 py-2 text-sm shadow-lg ring-1 ring-black/5 backdrop-blur-lg">
+              <Sparkle className="size-4 fill-[#9D86FF] text-[#9D86FF]" />
+              Available For Work
+            </div>
+            <h1 className="mt-4 text-4xl leading-[1.1] font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
+              Your Creative Partner Helping Businesses Scale With Confidence.
+            </h1>
+            <p className="my-4 max-w-xl text-base leading-relaxed text-white/70 sm:text-lg">
+              We create websites, content, designs, and marketing solutions
+              tailored to your brand’s goals.
+            </p>
+            <div className="flex gap-4">
+              <button className="text-pitch-deep transform cursor-pointer rounded-full bg-white px-6 py-2.5 text-sm font-semibold transition-all duration-300 hover:scale-105">
+                Let's Talk
+              </button>
+              <button className="transform cursor-pointer rounded-full bg-[linear-gradient(153.09deg,rgba(255,255,255,0.12)_16.83%,rgba(255,255,255,0.08)_85.44%)] px-6 py-2.5 text-sm font-semibold transition-all duration-300 hover:scale-105">
+                Our Works
+              </button>
+            </div>
+          </div>
+          <div className="w-full">
+            <img src="/images/hero.png" alt="hero image" className="w-full" />
+          </div>
+        </section>
+      </div>
+
+      {/* Services */}
+      {/* <div className="bg-linear-to-b from-[#9D86FF] to-[#7D52FD] py-2"> */}
+      <div className="flex w-full overflow-hidden bg-white py-4 whitespace-nowrap">
+        <Marquee autoFill pauseOnHover>
+          {[
+            "Social Media Management",
+            "Brand Design",
+            "Content Writing",
+            "UI/UX Design",
+            "Website Development",
+          ].map((service, idx) => (
+            <div
+              key={idx}
+              className="text-pitch-deep flex shrink-0 items-center gap-2 px-10 text-base"
+            >
+              <Sparkle className="size-4 fill-[#9D86FF] text-[#9D86FF]" />
+              {service}
+            </div>
+          ))}
+        </Marquee>
+      </div>
+      {/* </div> */}
+
+      {/* About */}
+      {/* <section className="main-background mx-auto py-24">
+        <div className="relative flex size-100 items-center justify-center">
+          <div className="absolute z-10 size-75 rounded-full bg-white" />
+          <img
+            src="/images/about.png"
+            alt="Person"
+            className="relative -top-20 z-20 w-full object-contain"
+          />
+        </div>
+      </section> */}
+    </section>
   );
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Brand from "./brand";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +14,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-6 left-1/2 z-50 w-[90%] max-w-5xl -translate-x-1/2 rounded-full border border-white/10 bg-white/5 px-6 py-3 shadow-lg ring-1 ring-black/5 backdrop-blur-lg transition-all duration-300 md:rounded-none md:border-none md:bg-transparent md:px-0 md:py-0 md:shadow-none md:ring-0 md:backdrop-blur-none">
+    <nav className="fixed top-6 left-1/2 z-50 mx-auto w-[96%] -translate-x-1/2 rounded-full border border-white/10 bg-white/5 px-4 py-3 shadow-lg ring-1 ring-black/5 backdrop-blur-lg transition-all duration-300 md:rounded-none md:border-none md:bg-transparent md:px-6 md:py-0 md:shadow-none md:ring-0 md:backdrop-blur-none">
       <div className="flex items-center justify-between">
         {/* Logo */}
         <div>
@@ -45,7 +46,7 @@ const Navbar = () => {
             href="#contact"
             className="text-pitch-deep transform rounded-full bg-white px-6 py-2.5 text-sm font-semibold transition-all duration-300 hover:scale-105 hover:bg-gray-100"
           >
-            HIRE US
+            Hire Us
           </a>
         </div>
 
@@ -54,28 +55,7 @@ const Navbar = () => {
           onClick={() => setIsOpen(!isOpen)}
           className="p-2 text-white focus:outline-none md:hidden"
         >
-          <svg
-            className="h-6 w-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            {isOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            )}
-          </svg>
+          {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
         </button>
       </div>
 
